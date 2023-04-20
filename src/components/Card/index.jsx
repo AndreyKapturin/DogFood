d6949383570d76901971265c07aa288019f429e6
Andrey Kapturin <andruxa21097@gmail.com>
2023-04-20 14:07:01 +0700

    Вертска катрочек

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css"
+import Like from "../images/Like";
 
 const Card = ({ product }) => {
-    const { name, picture, price, wight, discount } = product;
+    const { name, picture, price, wight, discount, isFavorite } = product;
+    console.log(product);
     return (
         <div className="test">
             {!!discount && <span className="card__discount">{discount} %</span>}
+            {!isFavorite && <Like />}
             <img className="card__img" src={picture} alt={name}></img>
             {!!discount ? <span className="card__old-price">{price} ₽</span> : <span className="card__old-price"></span>}
             {!!discount ? <span className="card__price red">{price - (price * discount) / 100} ₽</span> : <span className="card__price black">{price - (price * discount) / 100} ₽</span>}
